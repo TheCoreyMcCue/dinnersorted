ae2c4cee750bcf8f51dc3e84cc06d1fe86301af7
Corey McCue <coreysmccue@gmail.com>
2024-07-03 12:37:58 +0200

    user creation

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
-  
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-  
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-      dbName: 'recipeDB',
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+  console.log("here in database connection");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "recipeDB",
       bufferCommands: false,
-    })
-  
-    cached.conn = await cached.promise;
-  
-    return cached.conn;
-  }
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
