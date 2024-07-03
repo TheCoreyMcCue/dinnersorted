1cdc653dd7dc3d00c2d548f04a7427ed0f148a79
Corey McCue <coreysmccue@gmail.com>
2024-07-03 15:13:19 +0200

    user creation

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(evt.data);
+  console.log("event data heeerre", evt.data);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
